ab0db2ca38826f6116a2ac6ce3fff55fa8a395a2
rchDev <rizvan.chalilovas@proton.me>
2025-09-23 11:53:48 +0300

    Add BroadcastReceiver for battery events

--- app/src/main/java/com/example/lab07/MainActivity.java
+++ app/src/main/java/com/example/lab07/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.lab07;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int percent = (int) ((level / (float) scale) * 100);
+                Toast.makeText(context, "Battery level: " + percent, Toast.LENGTH_SHORT).show();
+            } else if (Intent.ACTION_BATTERY_LOW.equals(action)) {
+                // TODO: implement ACTION_BATTERY_LOW branch
+            } else if (Intent.ACTION_BATTERY_OKAY.equals(action)) {
+                // TODO: implement ACTION_BATTERY_OKAY branch
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +43,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction(Intent.ACTION_BATTERY_OKAY);
+
+        registerReceiver(batteryInfoReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(batteryInfoReceiver);
     }
 }
